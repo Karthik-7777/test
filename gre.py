2a9dd393276bc3d401d4901ee3c83a1b7d92b7fa
Karthik-7777 <93259037+Karthik-7777@users.noreply.github.com>
2021-10-29 10:13:47 +0530

    Add files via upload

---
+++ gre.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import ttk
+win=tk.Tk()
+win.title("GUI")
+
+name_label=ttk.Label(win,text="Name:-")
+name_label.grid(row=0,column=0,sticky=tk.W)
+
+name_label=ttk.Label(win,text="Name:-")
+name_label.grid(row=0,column=0,sticky=tk.W)
+
+
+name_label=ttk.Label(win,text="Name:-")
+name_label.grid(row=0,column=0,sticky=tk.W)
